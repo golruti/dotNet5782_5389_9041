283cd74f45340aa07829717f727fb452f1a25ddb
ruti goldfinger <golruti@gmail.com>
2021-10-06 19:50:32 +0300

    welcom function

---
+++ Targil0/program9041.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    class program
+    {
+        static partial void wwelcom9041()
+        {
+            Console.WriteLine("I'm also here");
+        }
+    }
+}
