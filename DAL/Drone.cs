55c9a964dc0612ca00a32b10d781ef9faf3444d5
golruti <golruti@gmail.com>
2021-10-12 18:08:31 +0300

    add classes

---
+++ DAL/Drone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        class Drone
+        {
+            public int id { get; set; }
+            public string model { get; set; }
+            public int maxWeight { get; set; }
+            public bool status { get; set; }
+            public double battery { get; set; }
+        }
+    }
+}
