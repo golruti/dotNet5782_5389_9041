99303e63cdf75b744cb0c196ab8d24af704d3527
golruti <golruti@gmail.com>
2021-10-12 18:08:31 +0300

    add classes

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDAL.DO.BaseStation = new IDAL.DO.BaseStation();
+            Console.WriteLine(baseStation);
+        }
+    }
+}
---
+++ DAL/DroneCharge.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        class DroneCarge
+        {
+           public int droneId { get; set; }
+            public int stationId { get; set; }
+        }
+    }
+}
