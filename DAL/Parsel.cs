94158efa4a11b80cb4195d4fd35b1b33641ceb90
ChayaAnkohitmachut <chaya.anko@gmail.com>
2021-10-13 14:59:26 +0300

    add classes

---
+++ DAL/Parsel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    namespace DO
+    {
+        public struct Parsel
+        {
+
+            public int Id { get; set; }
+            public int SenderId { get; set; }
+            public int TargetId { get; set; }
+
+
+
+        }
+    }
+}
