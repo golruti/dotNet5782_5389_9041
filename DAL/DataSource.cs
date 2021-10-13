6e911ff1ce1430170f30156c217de3a045c58496
golruti <golruti@gmail.com>
2021-10-13 18:12:18 +0300

    add class dal object

---
+++ DAL/DataSource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDAL.DO;
+
+namespace DalObject
+{
+    class DataSource
+    {
+        static internal Drone[] drones = new Drone[10];
+        static internal Drone[] stations = new Drone[5];
+        static internal Drone[] customers = new Drone[100];
+        static internal Drone[] parseles = new Drone[1000];
+    }
+}
