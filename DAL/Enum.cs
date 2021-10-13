49113726e9c4fb1ce1970ea687091289bffe2281
golruti <golruti@gmail.com>
2021-10-13 17:17:04 +0300

    add enum class

---
+++ DAL/Enum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        class Enum
+        {
+            public enum WeightCategories
+            {
+                light,
+                heavy,
+                medium
+            }
+
+            public enum Priorities
+            {
+                regular,
+                fast,
+                emergency
+            }
+
+            public enum DroneStatuses
+            {
+                available,
+                maintenance,
+                delivery
+            }
+
+          
+        }
+    }
+}
