a37e95b231ba7493e97eb3a3398ddc43eaacdade
ChayaAnkohitmachut <chaya.anko@gmail.com>
2021-10-19 15:58:14 +0300

    note

--- ConsoleUI/MainFunction.cs
+++ ConsoleUI/MainFunction.cs
@@ -3,12 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DalObject.DalObject;
 
+//הדף של הפונקציות לא מכיר את המחלקות האחרות והמיין לא מכיר את פונקציות ההכנסה\העדכון וכו
 namespace ConsoleUI
 {
    public class MainFunction
     {
         public static int xx = 10;
 
+       public Station getStation()
+        {
+            int tempID, tempChargeSlote;
+            string tempName;
+            double tempLongitude, tempLattitude;
+            Station tempStation = new Station();
+            Console.Write("Enter id");
+            tempID = int.Parse(Console.ReadLine());
+            tempStation.Id = tempID;
+
+            Console.Write("Enter name");
+            tempName = Console.ReadLine();
+            tempStation.Name = tempName;
+
+            Console.Write("Enter longitude");
+            tempLongitude = double.Parse(Console.ReadLine());
+            tempStation.Longitude = tempLongitude;
+
+            Console.Write("Enter lattitude");
+            tempLattitude = double.Parse(Console.ReadLine());
+            tempStation.Lattitude = tempLattitude;
+
+
+            Console.Write("Enter chargeSlote");
+            tempChargeSlote = int.Parse(Console.ReadLine());
+            tempStation.ChargeSlote = tempChargeSlote;
+
+            return tempStation;
+        }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -44,7 +44,7 @@ namespace ConsoleUI
                         {
 
                             case 1:
-                                DalObject.DalObject.InsertStation(getStation());
+                                DalObject.InsertStation(MainFunction.getStation());
 
 
 
@@ -142,35 +142,7 @@ namespace ConsoleUI
                         break;
 
 
-                        Station getStation()
-                        {
-                            int tempID, tempChargeSlote;
-                            string tempName;
-                            double tempLongitude, tempLattitude;
-                            Station tempStation = new Station();
-                            Console.Write("Enter id");
-                            tempID = int.Parse(Console.ReadLine());
-                            tempStation.Id = tempID;
-
-                            Console.Write("Enter name");
-                            tempName = Console.ReadLine();
-                            tempStation.Name = tempName;
-
-                            Console.Write("Enter longitude");
-                            tempLongitude = double.Parse(Console.ReadLine());
-                            tempStation.Longitude = tempLongitude;
-
-                            Console.Write("Enter lattitude");
-                            tempLattitude = double.Parse(Console.ReadLine());
-                            tempStation.Lattitude = tempLattitude;
-
-
-                            Console.Write("Enter chargeSlote");
-                            tempChargeSlote = int.Parse(Console.ReadLine());
-                            tempStation.ChargeSlote = tempChargeSlote;
-
-                            return tempStation;
-                        }
+                        
                 }
 
 
