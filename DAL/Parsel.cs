9411d89fa67750454317fe2e4edd23a3fa2ebc17
ChayaAnko <91947731+ChayaAnko@users.noreply.github.com>
2021-10-21 21:44:40 +0300

    Update Parsel.cs
    ToString

--- DAL/Parsel.cs
+++ DAL/Parsel.cs
@@ -11,7 +11,6 @@ namespace IDAL
     {
         public struct Parsel
         {
-
             public int Id { get; set; }
             public int SenderId { get; set; }
             public int TargetId { get; set; }
@@ -22,8 +21,13 @@ namespace IDAL
             public DateTime Scheduled { get; set; }
             public DateTime PickedUp { get; set; }
             public DateTime Delivered { get; set; }
-
-
         }
+        
+        public override void ToString()
+        {
+             return "id: "+ base.Id + "Sender id: "+ base.SenderId +"Target id: "+ base.TargetId + "Weight: "+ base.Weight +
+                 "Priority: "+ base.Priority + "Requested: "+ base.Requested+ "Droneld: "+ base.Droneld + "Scheduled: "+ base.Scheduled  +
+                 "PickedUp: "+ base.PickedUp + "Delivered: "+ base.Delivered ;
+         } 
     }
 }
