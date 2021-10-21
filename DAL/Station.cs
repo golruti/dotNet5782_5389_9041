c6071b9133c239468ddc68853bea431893c31e0e
ChayaAnko <91947731+ChayaAnko@users.noreply.github.com>
2021-10-21 21:41:15 +0300

    Update Station.cs

--- DAL/Station.cs
+++ DAL/Station.cs
@@ -12,5 +12,10 @@ namespace IDAL
             public double Lattitude{get;set;}
             public int ChargeSlote{get;set;}
         }
+        public override void ToString()
+   {
+      return "id: "+ base.Id + "name: "+ base.nameId +"logitude: "+ base.Logitude + "lattitude: "+ base.Lattitude +
+          "charge slote: "+ base.ChargeSlote ;
+   } 
     }
 }
