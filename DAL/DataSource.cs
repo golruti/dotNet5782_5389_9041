a0d8d5b5334aea057987977a410609387a9cd5c3
golruti <golruti@gmail.com>
2021-10-26 16:58:49 +0300

    change to list's push

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -15,7 +15,7 @@ namespace DalObject
         static internal List<Drone> drones = new List<Drone>();
         static internal List<Station> stations = new List<Station>();
         static internal List<Customer> customers = new List<Customer>();
-        static internal List<parcel> parcels = new List<parcel>();
+        static internal List<Parcel> parcels = new List<Parcel>();
         static internal List<DroneCharge> droneCharges = new List<DroneCharge>();
         internal class Config
         {
@@ -30,41 +30,40 @@ namespace DalObject
             //stations
             for (int i = 0; i < 2; ++i)
             {
-                Station temp
-                Station station=new Station();
-                Station.Id = Config.IndStation;
-                Station.Name = $"station {Config.IndStation}";
-                Station.ChargeSlote = Rand.Next() + 1;
-                Station.Lattitude = Rand.Next(181) + Rand.NextDouble();
-                Station.Longitude = Rand.Next(91) + Rand.NextDouble();
-                stations.push(station);
-                ++Config.IndStation;
+                Station tempStation = new Station();
+                tempStation.Id = i;
+                tempStation.Name = $"station {i}";
+                tempStation.ChargeSlote = Rand.Next() + 1;
+                tempStation.Lattitude = Rand.Next(181) + Rand.NextDouble();
+                tempStation.Longitude = Rand.Next(91) + Rand.NextDouble();
+                stations.Add(tempStation);
+               
             }
 
             //customers
             for (int i = 0; i < 10; ++i)
             {
-                Customer customer=new Customer();
-                customer.Id = Config.IndCustomer;
-                customer.Name = $"customer {Config.IndCustomer}";
-                customer.Phone = $"05{Rand.Next(10000000, 100000000)}";
-                customer.Lattitude = Rand.Next(181) + Rand.NextDouble();
-                customer.Longitude = Rand.Next(91) + Rand.NextDouble();
-                ++Config.IndCustomer;
-                customers.Add(new Customer() { Id =5, Name = "jj" });
+                Customer tempCustomer=new Customer();
+                tempCustomer.Id = i;
+                tempCustomer.Name = $"customer {i}";
+                tempCustomer.Phone = $"05{Rand.Next(10000000, 100000000)}";
+                tempCustomer.Lattitude = Rand.Next(181) + Rand.NextDouble();
+                tempCustomer.Longitude = Rand.Next(91) + Rand.NextDouble();     
+                customers.Add(tempCustomer);
             }
 
             //drones
             for (int i = 0; i < 5; ++i)
             {
-                drones[Config.IndDrone].Id = Config.IndDrone;
-                drones[Config.IndDrone].Model = $"drone {Config.IndDrone}";
-                drones[Config.IndDrone].MaxWeight = (IDAL.DO.Enum.WeightCategories)Rand.Next(0, 3);
-                drones[Config.IndDrone].Status = (IDAL.DO.Enum.DroneStatuses)Rand.Next(0, (int)IDAL.DO.Enum.DroneStatuses.Delivery);
-                drones[Config.IndDrone].Battery = Rand.Next(101);
-                ++Config.IndDrone;
+                Drone tempDrone = new Drone();
+                tempDrone.Id = i;
+                tempDrone.Model = $"drone {i}";
+                tempDrone.MaxWeight = (IDAL.DO.Enum.WeightCategories)Rand.Next(0, 3);
+                tempDrone.Status = (IDAL.DO.Enum.DroneStatuses)Rand.Next(0, (int)IDAL.DO.Enum.DroneStatuses.Delivery);
+                tempDrone.Battery = Rand.Next(101);
+                drones.Add(tempDrone);
 
-                customers.Add(new Customer() { Id = 5, Name = "jj" });
+           
             }
 
             //parcel
@@ -72,7 +71,7 @@ namespace DalObject
             {
                 Parcel tempParcel = new Parcel();
 
-                tempParcel.Id = il;
+                tempParcel.Id = i;
                 tempParcel.SenderId = Rand.Next(customers.Count());
                 tempParcel.TargetId = Rand.Next(customers.Count());
                 while (tempParcel.SenderId == tempParcel.TargetId)
@@ -81,7 +80,7 @@ namespace DalObject
                 }
                 tempParcel.Weight = (WeightCategories)(Rand.Next(0, 3));
                 tempParcel.Priority = (Priorities)(Rand.Next(0, 3));
-                tempParcel.Droneld = Config.IndParcel;
+                tempParcel.Droneld = i;
                 tempParcel.Requested = DateTime.Now;
                 tempParcel.Scheduled = DateTime.Now.AddDays(2);
                 tempParcel.PickedUp = DateTime.Now.AddDays(30);
