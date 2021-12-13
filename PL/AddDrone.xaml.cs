7dff540a8680bd476a7eff5d717805fbb6ab21c0
golruti <golruti@GMAIL.COM>
2021-12-14 00:18:28 +0200

    שגג בישמעקד

---
+++ PL/AddDrone.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for AddDrone.xaml
+    /// </summary>
+    public partial class AddDrone : Window
+    {
+       
+        IBL.IBL bl;
+        public AddDrone(IBL.IBL bl)
+        {
+            InitializeComponent();
+            this.bl = bl;
+            
+
+        }
+
+       private void AddTheDrone()
+        {
+
+            bl.AddDrone(int.Parse(Id.Text), int.Parse(StationsId.Text), (IBL.BO.Enums.WeightCategories)DroneWeights.SelectedItem, Model.Text);
+
+        }
+
+        //private void DroneWeights_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+
+        //}
+    }
+}
