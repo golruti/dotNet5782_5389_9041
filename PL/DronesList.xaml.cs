3d3cb712e61911839a6dcdc6dd7c7da36635849e
golruti <91947735+golruti@users.noreply.github.com>
2021-12-13 18:36:56 +0200

    add

---
+++ PL/DronesList.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using IBL.BO;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for DroneList.xaml
+    /// </summary>
+    public partial class DronesList
+    {
+        IBL.IBL bl;
+        public DronesList(IBL.IBL bl)
+        {           
+            InitializeComponent();
+            this.bl = bl;
+            DronesListView.DataContext = bl.GetDroneForList();
+            DroneWeights.DataContext = Enum.GetValues(typeof(Enums.WeightCategories));
+            //DroneStatuses.DataContext = Enum.GetValues(typeof(Enums.DroneStatuses));
+
+        }
+    }
+}
