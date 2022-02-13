0dffa1d42fae780cb6df5766df9632a0b5cdb55e
chaya.anko@gmail.com <chaya.anko@gmail.com>
2022-02-13 02:41:54 +0200

    Delete unnecessary

--- PL/ViewModel/BaseStationViewModel.cs
+++ PL/ViewModel/BaseStationViewModel.cs
@@ -8,18 +8,18 @@ namespace PL.ViewModel
 {
     public class BaseStationViewModel
     {
-        public PO.BaseStation SourceBaseStationInList { get; set; }
+        //public PO.BaseStation SourceBaseStationInList { get; set; }
 
         public PO.BaseStation BaseStationInList { get; set; }
-        public DroneInChargeListViewModel DronesInChargingList { get; set; }
+       // public DroneInChargeListViewModel DronesInChargingList { get; set; }
 
 
         public BaseStationViewModel(BO.BaseStationForList baseStationForList)
         {
 
-            SourceBaseStationInList = PO.ConvertFunctions.BOBaseStationToPO(ListsModel.Bl.GetBLBaseStation(baseStationForList.Id));
+            //SourceBaseStationInList = PO.ConvertFunctions.BOBaseStationToPO(ListsModel.Bl.GetBLBaseStation(baseStationForList.Id));
             BaseStationInList = PO.ConvertFunctions.BOBaseStationToPO(ListsModel.Bl.GetBLBaseStation(baseStationForList.Id));
-            DronesInChargingList = new DroneInChargeListViewModel(SourceBaseStationInList.Id);
+            //DronesInChargingList = new DroneInChargeListViewModel(SourceBaseStationInList.Id);
         }
 
         public BaseStationViewModel()
