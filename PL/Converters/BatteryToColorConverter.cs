d7aca6195bc8a31d403d85305b0b543535843023
dasizis <dasizisman480@gmail.com>
2022-02-17 16:51:56 +0200

    battery converter

---
+++ PL/Converters/BatteryToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Converters
+{
+    public class BatteryToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value < 10 ? "Red" : "#512da8";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
