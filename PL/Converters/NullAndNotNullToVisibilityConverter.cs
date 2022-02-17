57c621abc1226aa2e197ff141a6ba8b30553fb8b
dasizis <dasizisman480@gmail.com>
2022-02-18 00:42:17 +0200

    style

---
+++ PL/Converters/NullAndNotNullToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Converters
+{
+    class NullAndNotNullToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values[0] != null && values[1] == null? Visibility.Visible: Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
